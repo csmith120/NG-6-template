2ef85a62cddb4d1a1513d39efd56b713ca9773db
csmith120 <taskmaster3654@gmail.com>
2024-12-04 14:20:13 -0600

    nicer looking code

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,10 @@
-import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  afterNextRender,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { debounceTime } from 'rxjs';
 
@@ -15,17 +21,32 @@ export class LoginComponent {
 
   constructor() {
     afterNextRender(() => {
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email})),
-      });
+      const savedForm = window.localStorage.getItem('saved-login-form');
+
+      if (savedForm) {
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail);
+        }, 1);
+      }
+
+      const subscription = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) =>
+            window.localStorage.setItem(
+              'saved-login-form',
+              JSON.stringify({ email: value.email })
+            ),
+        });
 
       this.destroyRef.onDestroy(() => subscription?.unsubscribe());
-
     });
   }
 
   onSubmit(formData: NgForm) {
-    if (formData.form.invalid){
+    if (formData.form.invalid) {
       return;
     }
 
